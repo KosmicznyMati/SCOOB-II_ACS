43c7e293c049b09b767b96acbe3a51da6b62fb15
KosmicznyMati <56720774+KosmicznyMati@users.noreply.github.com>
2021-03-28 13:38:14 +0800

    Add files via upload

--- The_FOR_Loop.m
+++ The_FOR_Loop.m
@@ -15,10 +15,12 @@ omega_dot_array = zeros(n+1,3);
 q_error_array = zeros(n+1,4);
 omega_error_array = zeros(n+1,3);
 
+omega_rw_array = zeros(n+1,3);
+
 % Inputing initial conditions into the arrays
 q_array(1,:) = q_initial(:);
 omega_array(1,:) = omega_initial(:);
-
+omega_rw_array(1,:) = omega_rw_initial(:);
 
 % The Loop
 
@@ -26,6 +28,7 @@ for count = 1:n
     % Extracting the current orientation parameters from the array
     q = q_array(count,:)';          % Extracting the current attitude quaternion
     omega = omega_array(count,:)';  % Extracting the current omega
+    omega_rw= omega_rw_array(count,:)';
     
     % Normalising the current quaternion (move out of for loop? or leave for redundancy?
     q = Q_Normalise(q);
@@ -37,13 +40,17 @@ for count = 1:n
     % The control law PD (based on Sarthak's pic, it works better, don't know why)
     T = K_p*q_error(2:end)*q_error(1) + K_d*omega_error;  % Control torque vector
     
-    %Runge-Kutta Fourth Order numerical integration, output q normalisation
-    [omega_new,q_new] = RK4(J,J_inv,q,omega,dt,T);
+    % Decomposition into RWs
+    [H,omega_rw_new] = reaction_wheels(J_RW,rw_align,omega_rw,T,dt);
+    
+    % Runge-Kutta Fourth Order numerical integration, output q normalisation
+    [omega_new,q_new] = RK4(J,J_inv,H,q,omega,dt,T);
     q_new = Q_Normalise(q_new);
     
     % Storing the new quaternion & omega back in arrays
     q_array(count+1,:) = q_new(:);
     omega_array(count+1,:) = omega_new(:);
+    omega_rw_array(count+1,:) = omega_rw_new(:);
 end
 
 figure;
@@ -54,3 +61,7 @@ figure;
 plot(q_array)
 title('Attitude Quaternion')
 legend('q_0','q_1','q_2','q_3')
+figure;
+plot(omega_rw_array)
+title('Angular Velocity of RWs')
+legend('Omega_x','Omega_y','Omega_z')
