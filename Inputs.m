9fe1a9a5dee7647460a1125eac374e7c824b92a6
KosmicznyMati <56720774+KosmicznyMati@users.noreply.github.com>
2021-05-18 13:19:42 +0800

    Add files via upload

--- Inputs.m
+++ Inputs.m
@@ -7,41 +7,44 @@ addpath 'C:\Users\Mat\Desktop\FYP\matlab & simulink\Functions'
 % Spacecraft
 J = [0.0298629 0.0001670 0.000035;
      0.0001670 0.0063248 0.0000685;
-     0.000035  0.0000685 0.0306925];     % Moment of inertia of spacecraft kg-m^2
-% J = [10 0 0; 0 10 0; 0 0 10];
+     0.000035  0.0000685 0.0306925];                % Moment of inertia of spacecraft kg-m^2
+% J = eye(3);
 J_inv = inv(J);
 
 % Reaction Wheels
 J_RW = [0.00000820  0   0;
         0   0.00000820  0;
-        0   0   0.00001281];           % Moment of inertia of reaction wheel kg-m^2
+        0   0   0.00001281];                        % Moment of inertia of reaction wheel kg-m^2
     
-rw_align = eye(3);                  % Reaction wheels alignment matrix
+rw_align = eye(3);                                  % Reaction wheels alignment matrix
+k_n = 543;                                          % BLDC speed constant in min^-1/V
+rw_conversion = k_n*3.141592/30;                    % rad/s/V conversion factor
 
 % Controller
-K_p = 0.002;                          % Proportional gain; 0.2 for quaternion control; 0.002 for omega control 
-K_d = -0.001;                         % Differential gain; -0.1 for quaternion control; -0.001 for omega control 
+K_p = 0.2;                                          % Proportional gain; 0.2 for quaternion control;
+K_d = 0.1;                                          % Differential gain; 0.1 for quaternion control; (-) depending on error calc.
+K_p_w = 0.7;                                        %  0.7 for omega control 
+K_d_w = -0.005;                                     % -0.005 for omega control
 
 % Initial Conditions
-omega_rw_initial = [0 0 0]';        % Initial angular velocity of RWs
+omega_rw_initial = [0 0 0]';                        % Initial angular velocity of RWs
 
-omega_initial   = [0 0 0]';         % Initial angular velocity of system
-Direction_vector_q = [1 0 0]';      % Initial direction vector
-alpha = 0;                         % Initial angle in degrees
+omega_initial   = [0.01 0.01 0.01]';                % Initial angular velocity of system
+Direction_vector_q = [1 1 1]';                      % Initial direction vector
+alpha = 0;                                          % Initial angle in degrees
 
-unit_u_q = Unit(Direction_vector_q);% Unitising the direction vector
+unit_u_q = Unit(Direction_vector_q);                % Unitising the direction vector
 q_initial = [cosd(alpha); unit_u_q*sind(alpha)];    % Initial quaternion
 
-omega_dot_initial = [0 0 0]';       % Initial angular acceleration
+omega_dot_initial = [0 0 0]';                       % Initial angular acceleration
 
 % Desired Conditions
-omega_d         = [0.01 0.01 0.01]';                         % Desired omega
-Direction_vector_p = [1 0 0]';                          % Initial direction vector
-beta  = 0;                                             % Desired angle in degrees
+omega_d         = [0 0 0]';                         % Desired omega; if non-zero, timestep must be changed to 0.0001s
+Direction_vector_p = [1 0 0]';                      % Initial direction vector
+beta  = 40;                                         % Desired angle in degrees
 
-unit_u_p = Unit(Direction_vector_p);                    % Unitising the direction vector
-q_d = [cosd(beta); unit_u_p*sind(beta)];              % Desired quaternion
+unit_u_p = Unit(Direction_vector_p);                % Unitising the direction vector
+q_d = [cosd(beta); unit_u_p*sind(beta)];            % Desired quaternion
 
-% q_dot_d = [5 5 5 5]';                 % Rate of change of desired quaternion
-
-omega_dot_d = [0 0 0]';             % Desired angular acceleration
+% q_dot_d = [0.1 0.1 0 0]';                         % Rate of change of desired quaternion
+omega_dot_d = [0 0 0]';                             % Desired angular acceleration
--- The_FOR_Loop.m
+++ The_FOR_Loop.m
@@ -1,10 +1,9 @@
 
-
 % Definitions & Starting Parameters
 
 % Time
-n  = 10000;   % number of time steps
-dt = 0.01;    % time step in seconds
+n  = 1000;  % number of time steps
+dt = 0.01;    % time step in seconds; 0.0001 for omega control
 
 % Initialising arrays
 q_array = zeros(n+1,4);
@@ -18,38 +17,45 @@ q_error_array = zeros(n+1,4);
 omega_error_array = zeros(n+1,3);
 
 omega_rw_array = zeros(n+1,3);
+voltage_rw_array = zeros(n+1,3);
 
 % Inputing initial conditions into the arrays
 q_array(1,:) = q_initial(:);
 omega_array(1,:) = omega_initial(:);
 omega_rw_array(1,:) = omega_rw_initial(:);
-q_d_array(1,:) = q_d(:);
+
+% q_d_array(1,:) = q_d(:);
 
 % The Loop
 
 for count = 1:n
     % Extracting the current orientation parameters from the array
-    q = q_array(count,:)';          % Extracting the current attitude quaternion
-    omega = omega_array(count,:)';  % Extracting the current omega
-    omega_rw= omega_rw_array(count,:)';
-    q_d = q_d_array(count,:)';      % Extracting current desired quaternion
+    q = q_array(count,:)';          
+    omega = omega_array(count,:)';  
+    omega_rw = omega_rw_array(count,:)';
+    voltage_rw = voltage_rw_array(count,:)';
+%     q_d = q_d_array(count,:)';      
     omega_dot = omega_dot_array(count,:)';
     
-    % Normalising the current quaternion (move out of for loop? or leave for redundancy?
+    % Normalising the current quaternion (move out of for loop? or leave for redundancy?)
     q = Q_Normalise(q);
     
     % Error computations
-%     q_error = Q_Error(q,q_d);                   % Attitude error quaternion (calculations based on Oland PD+ paper)
-    omega_error = omega_d - omega;              % Omega error
-    omega_dot_error = omega_dot_d - omega_dot;  % Omega dot error
+    q_error = Q_Error(q,q_d);                   % Attitude error quaternion (calculations based on Oland PD+ paper)
+    omega_error = omega_d - omega;              
+    omega_dot_error = omega_dot_d - omega_dot;  
     
+    if isequal(omega_d,[0; 0; 0])
     % The control law PD (based on Sarthak's pic, it works better, don't know why)
-%     T = K_p*q_error(2:end)*q_error(1) + K_d*omega_error;  % Control torque vector
+        T = K_p*q_error(2:end)*q_error(1) + K_d*omega_error;
+    else
     % Alt PD control law for non-zero desired omega
-    T = K_p*omega_error + K_d*omega_dot_error;
+        T = K_p_w*omega_error + K_d_w*omega_dot_error;
+    end
     
     % Decomposition into RWs
     [H,omega_rw_new] = reaction_wheels(J_RW,rw_align,omega_rw,T,dt);
+    voltage_rw_new = omega_rw_new/rw_conversion;
 
     % Runge-Kutta Fourth Order numerical integration, output q normalisation
     [omega_new,q_new,omega_dot_new] = RK4(J,J_inv,H,q,omega,dt,T);
@@ -63,28 +69,38 @@ for count = 1:n
     omega_array(count+1,:) = omega_new(:);
     
     omega_rw_array(count+1,:) = omega_rw_new(:);
+    voltage_rw_array(count+1,:) = voltage_rw_new(:);
     
 %     q_error_array(count,:) = q_error(:);
     omega_error_array(count,:) = omega_error(:);
     
 %     q_d_array(count+1,:) = q_d_new(:);
-    
     omega_dot_array(count+1,:) = omega_dot_new(:);
 end
 
+% Plotting :)
 figure;
 plot(omega_array)
 title('Angular Velocity')
 legend('Omega_x','Omega_y','Omega_z')
+xlabel('Time (0.01s)')
+ylabel('Angular Velocity (rad/s)')
 figure;
 plot(q_array)
 title('Attitude Quaternion')
 legend('q_0','q_1','q_2','q_3')
+xlabel('Time (0.01s)')
+ylabel('Magnitude')
 figure;
 plot(omega_rw_array)
 title('Angular Velocity of RWs')
 legend('Omega_x','Omega_y','Omega_z')
+xlabel('Time (0.01s)')
+ylabel('Angular Velocity (rad/s)')
 % figure;
 % plot(q_d_array)
 % title('Desired Quaternion')
-% legend('q_0','q_1','q_2','q_3')
+% legend('q_0','q_1','q_2','q_3') 
+% xlabel('Time (0.01s)')
+% ylabel('Magnitude')
+% control laws fun, label units, convert to deg/s, s, 
