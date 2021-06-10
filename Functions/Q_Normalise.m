079c74221a1a210935c52c86c9add66c39e86ceb
KosmicznyMati <56720774+KosmicznyMati@users.noreply.github.com>
2021-06-10 16:55:10 +0800

    Add files via upload

--- Functions/Q_Normalise.m
+++ Functions/Q_Normalise.m
@@ -5,3 +5,8 @@
 function norm = Q_Normalise(q)
 norm = q/Q_Norm(q);
 end
+
+% This function outputs the quaternion norm
+function norm = Q_Norm(q)
+norm = sqrt(q(1)^2 + q(2)^2 + q(3)^2 + q(4)^2); %   Square root of squares of all elements of a quaternion
+end
